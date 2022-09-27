a1ba2e97d871dc94d93a5ef38152149bf17ef999
Foster808 <lafoster@outlook.com>
2022-09-26 18:13:17 -0700

    edits from pr

--- HGAlibs.py
+++ HGAlibs.py
@@ -1,33 +1,30 @@
-""" The Healing Gem of Arcadia Mad Libs Generator """
+#The Healing Gem of Arcadia Mad Libs Generator
 
 word1 = input("Enter a word: ")
-pluralNoun = input("Enter a plural noun: ")
+pluralnoun = input("Enter a plural noun: ")
 word2 = input("Enter a second word: ")
 place = input("Enter your favorite place: ")
 adjective = input("Enter an adjective: ")
 word3 = input("Enter a third word: ")
 
-print ("----------YOUR HEALING GEM OF ARCADIA LIBS---------")
+print(f"----------YOUR HEALING GEM OF ARCADIA LIBS---------")
 
-print ("The healing gem and the",word1,"went along with", pluralNoun)
+print(f"The healing gem and the {word1} went along with {pluralnoun},")
 
-print ("Because a healing gem is no use without", word2,",")
+print(f"Because a healing gem is no use without {word2},")
 
-print ("Remember the healing gem",pluralNoun,"in",place,".")
+print(f"Remember the healing gem {pluralnoun} in {place}.")
 
-print ("Where the Arcadia weather is always",adjective,".")
+print(f"Where the Arcadia weather is always {adjective}.")
 
-print ()
+print()
 
-print ("You may think that The Healing Gem of Arcadia is",word3,",")
+print(f"You may think that The Healing Gem of Arcadia is {word3},")
 
-print ("And you might be correct. But if you listen deeply you'll really hear its magic.")
+print(f"And you might be correct. But if you listen deeply you'll really hear its magic.")
 
-print ("------------------------------------------")
+print(f"------------------------------------------")
 
-print ("------------THANKS FOR PLAYING!!!---------")
+print(f"------------THANKS FOR PLAYING!!!---------")
 
-print ("------------------------------------------")
-
-
-
+print(f"------------------------------------------")
